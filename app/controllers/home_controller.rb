b02c25eba1cfa4e8576821472f1b18334d07723f
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-11-15 17:46:01 -0500

    Refactored the scaffold to be consistent with the template designs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,6 @@ class HomeController < ApplicationController
   def get
   end
 
-  def table
-  end
-
-  def form
-  end
-
   def past
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :items
   get 'home/get'
-  get 'home/table'
-  get 'home/form'
+
   get 'home/past'
 
   root 'home#get'
