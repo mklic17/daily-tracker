b3171e0662e0d2c37ec415243455c5feda3571aa
reynathangray <reynathangray@gmail.com>
2016-11-15 19:19:29 -0500

    styled the activities table and create activity link

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -28,7 +28,7 @@ class ActivitiesController < ApplicationController
 
     respond_to do |format|
       if @activity.save
-        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
+        format.html { redirect_to "/activities", notice: 'Activity was successfully created.' }
         format.json { render :show, status: :created, location: @activity }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ActivitiesController < ApplicationController
   def update
     respond_to do |format|
       if @activity.update(activity_params)
-        format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
+        format.html { redirect_to "/activities", notice: 'Activity was successfully updated.' }
         format.json { render :show, status: :ok, location: @activity }
       else
         format.html { render :edit }
